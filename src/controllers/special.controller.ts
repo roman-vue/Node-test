61f1f5977fb764cb864cfba4ff08f17669f5c76b
Roman Calderin <55627800+roman-vue@users.noreply.github.com>
2021-07-27 12:47:26 -0600

    delete token

--- src/controllers/special.controller.ts
+++ src/controllers/special.controller.ts
@@ -4,4 +4,8 @@ export const special = (req: Request, res: Response) => {
   return res.json({ msg: `Hey ${req.body.email}!` });
 };
 
+export const remove = (req: Request, res: Response) => {
+  return res.json({ msg: `remove ${req.body.email}!` });
+}; 
+
 
